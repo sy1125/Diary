a1f86c21821e3b32936d4b5fd7f858eb59a53b48
Leeseonyong <qnfto7898@naver.com>
2022-06-12 23:27:13 +0900

    basework

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState, useRef } from "react";
+import './App.css';
+import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "이선용",
+//     content: "하이 1",
+//     emotion: 5,
+//     created_date: new Date().getTime()
+//   },
+//   {
+//     id: 2,
+//     author: "홍길동",
+//     content: "하이 2",
+//     emotion: 2,
+//     created_date: new Date().getTime()
+//   },
+//   {
+//     id: 3,
+//     author: "아무개",
+//     content: "하이 3",
+//     emotion: 1,
+//     created_date: new Date().getTime()
+//   },
+// ];
+
+function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  return (
+    <div className="App">
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
+    </div>
+  );
+}
+
+export default App;
