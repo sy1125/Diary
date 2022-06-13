bbeee7396d615f4c74094816133f6c2f17fcf764
LeeSeonyong <qnfto7898@naver.com>
2022-06-13 10:35:28 +0900

    delete data

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,4 +1,4 @@
-const DiaryItem = ({ author, content, created_date, emotion, id }) => {
+const DiaryItem = ({ onDelete, id, author, content, created_date, emotion }) => {
   return <div className="DiaryItem">
     <div className="info">
       <span>작성자 : {author} | 감정점수 : {emotion}</span>
@@ -6,6 +6,12 @@ const DiaryItem = ({ author, content, created_date, emotion, id }) => {
       <span className="date">{new Date(created_date).toLocaleString()}</span>
     </div>
     <div className="content">{content}</div>
+    <button onClick = {() => {
+      console.log(id);
+      if(window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+        onDelete(id);
+      }
+    }}>삭제하기</button>
   </div>
 };
 export default DiaryItem;
