ad57ce78dea5515b875a217b6f5ae9f9e69f9552
LeeSeonyong <qnfto7898@naver.com>
2022-06-13 13:34:33 +0900

    Initial value setting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
+// https://jsonplaceholder.typicode.com/comments
+
 
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async() => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments").then((res)=>res.json());
+    const initData = res.slice(0,20).map((it)=>{
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5)+1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      }
+    });
+    setData(initData);
+  };
+
+  useEffect(()=>{
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
